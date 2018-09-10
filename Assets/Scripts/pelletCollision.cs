32002505c26a9f428be2d6b9e2b2f66d5cf5309f
figgyfunk <figa@rpi.edu>
2018-09-10 13:30:18 -0400

    updated formatting and correct words.

---
+++ Assets/Scripts/pelletCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pelletCollision : MonoBehaviour {
+
+    private GameObject gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.gameObject.name == "Pellet(Clone)")
+        {
+            Destroy(collision.collider.gameObject);
+            gameManager.SendMessage("updateScore");
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/scoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreManager : MonoBehaviour {
+
+    private Text scoreText;
+    private Text highText; 
+    private int score;
+    private int highscore; 
+	// Use this for initialization
+	void Start ()
+    {
+        score = 0;
+        scoreText = GameObject.Find("Score").GetComponent<Text>();
+        scoreText.text = ("Game"+'\n' + "Score" + '\n' + string.Format("{0:0\n0\n0\n0\n0}", score));
+        
+        highText = GameObject.Find("HighScore").GetComponent<Text>();
+        if (PlayerPrefs.HasKey("highscore"))
+        {
+            highscore = PlayerPrefs.GetInt("highscore");
+        }
+        else
+        {
+            highscore = 0;
+        }
+        highText.text = "High"+"\n"+"Score" + '\n' + string.Format("{0:0\n0\n0\n0\n0}", highscore);
+    }
+	
+	public void updateScore()
+    {
+        score += 1;
+        scoreText.text = "Score" + '\n' + string.Format("{0:0\n0\n0\n0\n0}", score);
+    }
+
+    private void OnDestroy()
+    {
+        if(score > highscore)
+        {
+            PlayerPrefs.SetInt("highscore", score);
+            PlayerPrefs.Save();
+        }
+    }
+}
